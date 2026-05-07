fef1bd6ec73030d345afea0ecd65121efd0fa3f7
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-05-07 13:37:08 -0500

    Feat: getPointValue() correct values

--- src/main/java/com/pluralsight/Card.java
+++ src/main/java/com/pluralsight/Card.java
@@ -34,6 +34,30 @@ public class Card {
         // A = 11
         // K, Q, J = 10
         // all numeric cards are equal to their face value
+            switch (value) {
+                case "1":
+                    return 11;
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+                case "5":
+                    return 5;
+                case "6":
+                    return 6;
+                case "7":
+                    return 7;
+                case "8":
+                    return 8;
+                case "9":
+                    return 9;
+                case "10", "J", "Q", "K":
+                    return 10;
+                default:
+                    return 0;
+            }
         } else {
             return 0;
         }
